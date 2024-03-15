e2ee72c047779d13d4cdacc4da1a42eaa61733e2
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-15 21:55:06 +0200

    add textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function App() {
-  const [formData, setFormData] = useState({ firstName: "", lastName: "" });
+  const [formData, setFormData] = useState({ firstName: "", lastName: "",comment:"" });
   function handleChange(event) {
     setFormData((prevData) => {
       return { ...prevData, [event.target.name]: event.target.value };
@@ -24,6 +24,14 @@ export default function App() {
           onChange={handleChange}
           name="lastName"
           value={formData.lastName}
+        />
+
+        <textarea
+          type="text"
+          placeholder="comment"
+          onChange={handleChange}
+          name="comment"
+          value={formData.comment}
         />
       </form>
       <p>
