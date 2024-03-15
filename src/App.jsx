7695d694cf26beefff9e6096205efff3d65f7968
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-15 21:14:06 +0200

    handle an input

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
-  return "setup test";
+  const [firstName,setFirstName] = useState("");
+  function handleChange(event) {
+    setFirstName(event.target.value);
+    console.log(firstName);
+  }
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="First Name" onChange={handleChange} />
+      </form>
+      <p>Hello,{firstName}</p>
+    </>
+  );
 }
